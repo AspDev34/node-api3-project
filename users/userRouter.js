d285ea9dc41c71262aa85820c63359a3ffe60d22
Ike Otu <ikeotu@msn.com>
2020-04-09 12:05:53 -0700

    completed several routes and middlewares

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,28 +2,70 @@ const express = require('express');
 
 const router = express.Router();
 
+const Users = require('./userDb.js'); //imports the user database
+const Posts = require('../posts/postDb.js') // imports the post database
+
+
+//posts a new user
 router.post('/', (req, res) => {
-  // do your magic!
+  // /api/users
+  Users.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({message: `An error occurred when adding user: ${err}`})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+//creates a post from user
+router.post('/:id/posts', validateUserId, (req, res) => {
+  const newPost = {...req.body, user_id: req.user.id}
+  console.log(req.body)
+  Posts.insert(newPost)
+  .then(post => {
+    res.status(201).json(post)
+  })
+  .catch(err => {
+    res.status(500).json({message: `error adding post: ${err}`})
+  })
 });
 
+//gets list of users
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get(req.query)
+  .then(users => {
+    res.status(200).json({message: 'error retrieving users'})
+  })
+  .catch(err => {
+    res.status(500).json({message: `error retrieving users: ${err}`})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+//gets a specific user
+router.get('/:id', validateUserId, (req, res) => {
+  Users.getUserPosts(req.user.id)
+  .then(posts => res.status(200).json(posts))
+  .catch(err => {
+    res.status(500).json({message: `error retrieving user: ${err}`})
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+//get a user's posts
+router.get('/:id/posts', validateUserId, (req, res) => {
+  Users.getUserPosts(req.user.id)
+  .then(posts => res.status(200).json(posts))
+  .catch(err => {
+    res.status(500).json({message: `${err} error retrieving posts`})
+  })
 });
 
+//deletes a specific user
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Users.remove(req.user.id)
+  .then(user => {
+    res.status(200).json({message: `${user} deleted`})
+  })
 });
 
 router.put('/:id', (req, res) => {
