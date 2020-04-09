93743282df510982b534774f3c50b5afd4daff37
Ike Otu <ikeotu@msn.com>
2020-04-09 09:30:31 -0700

    middleware written and exported, imported to index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // code away!
-initial commit
+require('dotenv').config(); // require() is essentially like importing. Here I import dotenv and run a config function on it.
+
+const server = require('./server.js') // Here I am importing the server.js file for use in index.js, which is what the actual file node reads
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`\n the server is running on http://localhost:${port}`);
+})
